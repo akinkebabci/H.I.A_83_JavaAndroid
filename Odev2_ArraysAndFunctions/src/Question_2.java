757e0229c4e18a361901cfb0c03b69afd58603b8
Akın Kebabcı <kebabci12@gmail.com>
2022-12-29 19:00:08 +0300

    Homework ArraysAndFunctions updated

--- Odev2_ArraysAndFunctions/src/Question_2.java
+++ Odev2_ArraysAndFunctions/src/Question_2.java
@@ -1,22 +1,20 @@
-import java.util.Arrays;
+
 
 public class Question_2 {
     public static void main(String[] args) {
         int[] myArr = {8, 5, 7, 12, 15,78,54};
 
         int[] newArray = scrollRigthIndex(myArr);
-
     }
 
     static int[] scrollRigthIndex(int[] arr) {
 
         int[] newArray = new int[arr.length];
 
-        for (int i = 0; i < arr.length ; i++) {
-            for (int j = 0; j < i ; j++) {
-                newArray[i] = arr[j];
-                newArray[0] = arr[arr.length-1];
-            }
+        for (int i = 0; i < arr.length-1; i++) {
+            newArray[i+1] = arr[i];
+            newArray[0] = arr[arr.length-1];
+
         }
 
         return newArray;
